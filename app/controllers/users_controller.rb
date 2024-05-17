e46fa2809a457d2a259026f69f7ca4e66152ed56
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-17 14:02:11 +0000

    Userコントローラー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
-　def destroy
-　end 
+    
+  def index
+  end 
+  def edit
+  end 
+
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     devise_for :users
     
     resources :books, only:[:new, :index, :show, :edit, :create, :destroy]
-    
+    resources :users, only:[:index, :edit]
     get 'homes/about'=>"homes#about", as:'about'
     
 end
