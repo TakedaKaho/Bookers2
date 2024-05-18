19b479b6ad97639efcb127d3d46eece3d68d1ec6
EC2 Default User <ec2-user@ip-172-31-20-69.ap-northeast-1.compute.internal>
2024-05-18 03:53:17 +0000

    link_to追加　

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
     
   def index
+    #@books = User.find(params[:id])
+    @user = User.all #allでいいのか
+    @books = @user.name#いろんなユーザーのプロフィール画像と名前だけ出したい
   end 
   def edit
   end 
+  
+  def show
+  end 
 
+  def update
+  end
+  
 end 
