dd0f188a95e42a36eccfa509b322b08febde317b
TakedaKaho <tktk.a3104c@gmail.com>
2024-08-06 07:34:05 +0000

    地図表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+
     root to: "homes#top"
     get '/home/about', to: 'homes#about', as: 'about'
     devise_for :users
@@ -9,6 +10,6 @@ Rails.application.routes.draw do
     
     resources :books, only:[:new, :index, :show, :edit, :create, :destroy, :update]
     resources :users, only:[:index, :edit, :update, :show, :create]
-
+    resource :map, only: [:show]
  end
 
